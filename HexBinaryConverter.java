c6f2ce47c98e89d7e92207bdeea38dd4459fe48c
Maria <148421136+pincherMaria@users.noreply.github.com>
2023-10-26 20:18:45 +0600

    HexBinaryConverter_second_version
    Made this one closer to the task instuctions

--- HexBinaryConverter.java
+++ HexBinaryConverter.java
@@ -5,63 +5,102 @@
 public class HexBinaryConverter
 {
     private static final String HEX = "0123456789abcdef";
+    private static final char[] HEXarray = HEX.toCharArray();
 
     public static void main(String[] args)
     {
-        int binaryNumber = 12566;
-        //System.out.println("Двоичное число " + binaryNumber + " равно шестнадцатеричному числу " + toHex(binaryNumber));
-        String hexNumber = "528d9e";
-        System.out.println("Шестнадцатеричное число " + hexNumber + " равно двоичному числу " + toBinary(toBinary(hexNumber)));
+        String binaryNumber = "100111010000";
+        System.out.println("Двоичное число " + binaryNumber + " равно шестнадцатеричному числу " + toHex(binaryNumber));
+        String hexNumber = "9d0";
+        System.out.println("Шестнадцатеричное число " + hexNumber + " равно двоичному числу " + toBinary(hexNumber));
     }
 
-    public static String toBinary(int decimalNumber)
+    public static String toHex(String binaryNumber)
     {
-        if (decimalNumber <= 0) return "";
-
-        String binary = "";
-        while (decimalNumber != 0)
+        char[] binaryArray = binaryNumber.toCharArray();
+        //блок проверки аргумента на содержание подходящих символов
         {
-            binary += +decimalNumber % 2;
-            decimalNumber /= 2;
-        }
-
-        String result = "";
-        char[] array = binary.toCharArray();
-        for (int i = array.length - 1; i >= 0; i--)
-        {
-            result += array[i];
-        }
-        return result;
-    }
-
-    /*public static String toHex(int decimalNumber)
-    {
-        if (decimalNumber <= 0) return "";
-
-        String hex = "";
-        while (decimalNumber != 0)
-        {
-            hex = HEX.charAt(decimalNumber%16) + hex;
-            decimalNumber /= 16;
-        }
-        return hex;
-    }*/
-
-    public static int toBinary(String hexNumber)
-    {
-        if (hexNumber == null || hexNumber.equals("")) return 0;
-
-        int dec = 0;
-        for (int i = 0; i < hexNumber.length(); i++)
-        {
-            int HEXindex = 0;
-            for (int j = 0; j < HEX.length(); j++)
+            for (int i = 0; i < binaryArray.length; i++)
             {
-                if (HEX.charAt(j) == hexNumber.charAt(i))
-                { HEXindex = j; break; }
+                if (Character.getNumericValue(binaryArray[i]) != 0 && Character.getNumericValue(binaryArray[i]) != 1)
+                    return null;
             }
-            dec = 16 * dec + HEXindex;
-        }                     /*индекс символа из строки HEX, равного символу из входящей строки с индексом i*/
-        return dec;
+        }
+        //блок обеспечения кратности длины аргумента числу 4
+        {
+            while (binaryArray.length % 4 != 0)
+            {
+                invert(binaryArray);
+                String stringArray = arrayToString(binaryArray);
+                stringArray += "0";
+                binaryArray = stringArray.toCharArray();
+                invert(binaryArray);
+            }
+        }
+
+        //основной блок
+        {
+            String[] quarters = new String[binaryArray.length/4];
+            for (int i = 0; i < binaryArray.length; )
+            {
+                int k = 0;
+                for (int j = 0; j < i+4; j++)
+                {
+                    quarters[k  ] = String.valueOf(binaryArray[j]);
+                }
+                k++; i+=4;
+            }
+
+            for (int i = 0; i < quarters.length; i++)
+            {
+                System.out.println(quarters[i]);
+            }
+        }
+        return null;
+    }
+
+    public static String toBinary(String hexNumber)
+    {
+        //блок проверки аргумента на содержание подходящих символов
+        {
+            char[] array = hexNumber.toCharArray();
+            char[] hexArray = HEX.toCharArray();
+            String check = "";
+            for (int i = 0; i < array.length; i++)
+            {
+                for (int j = 0; j < hexArray.length; j++)
+                {
+                    if (array[i] == (hexArray[j]))
+                    {
+                        check += array[i];
+                        break;
+                    }
+                }
+            }
+            char[] checkArray = check.toCharArray();
+            if (checkArray.length != array.length)
+                return null;
+        }
+
+        return null;
+    }
+
+    static void invert(char[] array)
+    {
+        char[] array2 = array;
+        for (int i = 0; i < array.length; i++)
+        {
+            array2[i] = array[array.length-1-i];
+        }
+    }
+
+    static String arrayToString(char[] array)
+    {
+        String stringArray = "";
+        for (int i = 0; i < array.length; i++)
+        {
+            stringArray += array[i];
+        }
+        return stringArray;
     }
 }
